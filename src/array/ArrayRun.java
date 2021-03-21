4566a77c8c84ee34700b381da6231adaeea44e5a
gopal1895 <kumegopal9505@gmail.com>
2021-03-21 12:15:39 +0530

    Hello World

---
+++ src/array/ArrayRun.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		int[] a= {2,3,4,5,6};
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+
+	}
+
+}
