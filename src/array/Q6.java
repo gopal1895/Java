4c62f68fdb6f7288a5fb8669bd23abd13cf8d5c3
gopal1895 <kumegopal9505@gmail.com>
2021-03-21 14:56:45 +0530

    Wap find and display reverse of an array without using third variable.

---
+++ src/array/Q6.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		//Write a program to find and display reverse of an array without using third variable.
+		int[] a= {2,3,4,5,1,8,6};
+		for (int i = a.length-1;i>=0; i--) 
+		{
+			System.out.print(a[i]+" ");
+			
+		}
+
+	}
+
+}
